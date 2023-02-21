c92ec8dca99e0debb8f52d8bb7cb4c5309481196
Jichen Li <114197089+Jichen-Li@users.noreply.github.com>
2023-02-20 16:48:16 -0800

    test

---
+++ TestSignalDetection.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3.8
+
+import unittest
+import numpy as np
+from numpy import random
+from SignalDetection import SignalDetection
+
+class TestSignalDetection(unittest.TestCase):
+
+    def test_d_prime_zero(self):
+        sd   = SignalDetection(15, 5, 15, 5)
+        expected = 0
+        obtained = sd.d_prime()
+        # Compare calculated and expected d-prime
+        self.assertAlmostEqual(obtained, expected, places=10)
+
+    def test_d_prime_nonzero(self):
+        sd   = SignalDetection(15, 10, 15, 5)
+        expected = -0.421142647060282
+        obtained = sd.d_prime()
+        # Compare calculated and expected d-prime
+        self.assertAlmostEqual(obtained, expected, places=10)
+
+    def test_criterion_zero(self):
+        sd   = SignalDetection(5, 5, 5, 5)
+        # Calculate expected criterion
+        expected = 0
+        obtained = sd.criterion()
+        # Compare calculated and expected criterion
+        self.assertAlmostEqual(obtained, expected, places=10)
+
+    def test_criterion_nonzero(self):
+        sd   = SignalDetection(15, 10, 15, 5)
+        # Calculate expected criterion
+        expected = -0.463918426665941
+        obtained = sd.criterion()
+        # Compare calculated and expected criterion
+        self.assertAlmostEqual(obtained, expected, places=10)
+
+    def test_addition(self):
+        sd = SignalDetection(1, 1, 2, 1) + SignalDetection(2, 1, 1, 3)
+        expected = SignalDetection(3, 2, 3, 4).criterion()
+        obtained = sd.criterion()
+        # Compare calculated and expected criterion
+        self.assertEqual(obtained, expected)
+
+    def test_multiplication(self):
+        sd = SignalDetection(1, 2, 3, 1) * 4
+        expected = SignalDetection(4, 8, 12, 4).criterion()
+        obtained = sd.criterion()
+        # Compare calculated and expected criterion
+        self.assertEqual(obtained, expected)
+
+    def test_plot_roc(self):
+        rng = random.default_rng(seed = 3458)
+        rint = dict()
+        for i in range(4):
+            integer = rng.integers(1,20,10)
+            rint[i] = np.sort(integer)
+        sd = SignalDetection(rint[0], rint[1], rint[2], rint[3]) * 4
+        sd.plot_roc()
+
+    def test_plot(self):
+        sd = SignalDetection(180, 230, 22, 5) * 4
+        sd.plot_sdt()
+
+if __name__ == '__main__':
+    unittest.main()
