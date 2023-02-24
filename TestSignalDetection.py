8f060cf6f497e4136c691719756ad34738347667
Jichen-Li <jichel2@uci.edu>
2023-02-24 00:06:19 -0800

    test using only one set of data

--- TestSignalDetection.py
+++ TestSignalDetection.py
@@ -52,17 +52,25 @@ class TestSignalDetection(unittest.TestCase):
         self.assertEqual(obtained, expected)
 
     def test_plot_roc(self):
-        rng = random.default_rng(seed = 3680)
-        rint = dict()
-        for i in range(4):
-            integer = rng.integers(1,20,1000)
-            rint[i] = np.sort(integer)
-        sd = SignalDetection(rint[0], rint[1], rint[2], rint[3]) * 4
+        sd = SignalDetection(1, 2, 3, 1)
         sd.plot_roc()
 
     def test_plot_sdt(self):
-        sd = SignalDetection(180, 230, 22, 5) * 4
+        sd = SignalDetection(1, 2, 3, 1)
         sd.plot_sdt()
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
+### The code below is unrelated to hw3
+
+    # def test_plot_roc(self):
+    #     rng = random.default_rng(seed = 3680)
+    #     rint = dict()
+    #     for i in range(4):
+    #         integer = rng.integers(1,20,1000)
+    #         rint[i] = np.sort(integer)
+    #     sd = SignalDetection(rint[0], rint[1], rint[2], rint[3]) * 4
+    #     sd.plot_roc()
