6006f1dd93ddabab615c62a2406b7c5f2e698b6b
Jichen-Li <jichel2@uci.edu>
2023-03-21 13:05:08 -0700

    step() as an intermediate method

--- Metropolis.py
+++ Metropolis.py
@@ -8,3 +8,11 @@ class Metropolis:
         self.stepSize = stepSize
         self.samples = []
         self.acceptanceRate = 0.4
+
+    def step(self):
+        nextState = np.random.normal(loc=self.state, scale=self.stepSize)
+        logAcceptanceProb = self.logTarget(nextState) - self.logTarget(self.state)
+        if np.log(np.random.uniform()) < logAcceptanceProb:
+            self.state = nextState
+            self.samples.append(nextState)
+            self.acceptanceRate += 1.0 / len(self.samples)
