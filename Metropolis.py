4330ec0b6c52b1fa3c99866f842f57f82897ce2f
Jichen-Li <jichel2@uci.edu>
2023-03-21 13:10:07 -0700

    private _accept method

--- Metropolis.py
+++ Metropolis.py
@@ -9,6 +9,16 @@ class Metropolis:
         self.samples = []
         self.acceptanceRate = 0.4
 
+    def _accept(self, proposal):
+        log_ratio = self.log_target(proposal) - self.log_target(self.current_state)
+        acceptance_probability = np.exp(np.minimum(log_ratio, 0))
+        if np.random.uniform() < acceptance_probability:
+            self.current_state = proposal
+            self.accepted_proposals += 1
+            return True
+        else:
+            return False
+
     def step(self):
         nextState = np.random.normal(loc=self.state, scale=self.stepSize)
         logAcceptanceProb = self.logTarget(nextState) - self.logTarget(self.state)
